90b162330f274f2fd1491586223fcf786b059488
trevenue44 <trevenue44.main@gmail.com>
2023-04-24 13:23:21 +0000

    project setup - vite, react, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-blue-500 text-center">
+        Let's Laugh!
+      </h1>
+    </>
+  );
+}
+
+export default App;
